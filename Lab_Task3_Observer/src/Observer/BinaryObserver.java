397b68a92637036644b23de695ca33f0986e83c8
Ghulam Abbas <102281059+shani710@users.noreply.github.com>
2023-09-22 10:23:34 +0500

    Observer Example
    Implement the observer example.
    lab task not complete still I am working on it.

---
+++ Lab_Task3_Observer/src/Observer/BinaryObserver.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Observer;
+
+/**
+ *
+ * @author FA20-BSE-053
+ */
+public class BinaryObserver extends Observer{
+
+   public BinaryObserver(Subject subject){
+      this.subject = subject;
+      this.subject.attach(this);
+   }
+
+   @Override
+   public void update() {
+      System.out.println( "Binary String: " + Integer.toBinaryString( subject.getState() ) ); 
+   }
+}
---
+++ Lab_Task3_Observer/src/Observer/HexaObserver.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Observer;
+
+/**
+ *
+ * @author FA20-BSE-053
+ */
+public class HexaObserver extends Observer{
+
+   public HexaObserver(Subject subject){
+      this.subject = subject;
+      this.subject.attach(this);
+   }
+
+   @Override
+   public void update() {
+      System.out.println( "Hex String: " + Integer.toHexString( subject.getState() ).toUpperCase() ); 
+   }
+}
