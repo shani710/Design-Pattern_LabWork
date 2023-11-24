aa61e473b36f8b1b1d18a3c64403879563d0f1b9
Ghulam Abbas <102281059+shani710@users.noreply.github.com>
2023-11-24 10:33:49 +0500

    command

---
+++ GOF/Command/src/TutorialPointExample/CommandPatternDemo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TutorialPointExample;
+/**
+ *
+ * @author FA20-BSE-053
+ */
+public class CommandPatternDemo {
+   public static void main(String[] args) {
+      Stock abcStock = new Stock();
+
+      BuyStock buyStockOrder = new BuyStock(abcStock);
+      SellStock sellStockOrder = new SellStock(abcStock);
+
+      Broker broker = new Broker();
+      broker.takeOrder(buyStockOrder);
+      broker.takeOrder(sellStockOrder);
+
+      broker.placeOrders();
+   }
+}
